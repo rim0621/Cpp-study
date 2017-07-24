db5d3d82876201b99156a1d5a4112cbc6feb5d0e
rim0621 <jeremy9106@gmail.com>
2017-07-24 23:28:08 +0900

    2017-07-24

---
+++ 6.sequence_container/vectorReserve.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> v;
+	v.reserve(8);
+	cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+	v.push_back(10);
+        cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+        v.push_back(20);
+	cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+        v.push_back(30);
+	cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+        v.push_back(40);
+	cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+        v.push_back(50);
+	cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+        v.push_back(59);
+	cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+	v.push_back(41);      
+	cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+        v.push_back(60);
+        cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+        v.push_back(70);
+        cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+        v.push_back(80);
+        cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+        v.push_back(99);
+        cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+        v.push_back(111);
+	cout<<"size:"<<v.size()<<" capacity:"<<v.capacity()<<endl;
+	return 0;
+}	
